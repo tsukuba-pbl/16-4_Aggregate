db0bd51c12a732d51adf1efc646972d450520a53
ferretdayo <ferretdayo@hotmail.co.jp>
2016-10-12 19:23:00 +0900

    クラス化出来たが、エラー処理がいまいち出来ていない

--- public/FileHandle.php
+++ public/FileHandle.php
@@ -2,12 +2,16 @@
     class FileHandle{
         private $filehandler;
         private $filename;
-        private $dirname;
-        public function __construct($dirname, $filename){
+
+        public function __construct($filename = ""){
             $this->filename = $filename;
-            $this->dirname = $dirname;
         }
-        private function fopenRead($filename = $this->filename){
+        /*
+            ファイルを読み取り専用でオープン
+            return 配列
+        */
+        public function read($filename){
+            $fileData = []; //ファイルのデータを1行ごとに保持する配列
             try{
                 if(file_exists($filename)){
                     $this->filehandler = fopen($filename, "r");
@@ -17,12 +21,21 @@
             } catch (Exception $e) {
                 echo $e->getMessage();
             }
-        }
-        public function read(){
-            private $fileData = [];
+            //ファイルの内容を1行ずつ配列に格納
             while($line = fgets($this->filehandler)){
                 $fileData[] = $line;
             }
-            reutrn $fileData;
+            fclose($this->filehandler);
+            return $fileData;
+        }
+
+        /*
+            ファイルに新規書き込み(上書きしない)
+        */
+        public function initWrite($filename, $data){
+            if(!file_exists($filename)){
+                touch($filename);
+            }
+            file_put_contents($filename, $data);
         }
     }
--- public/download.php
+++ public/download.php
@@ -1,20 +1,9 @@
 <?php
-    //csvのあるフォルダ
-    define("TARGET_DIR", "./csv/");
     //結果を書き込むファイル
     define("DIST_FILE", "./csv/result.csv");
-    /*
-        1行のCSVデータを分割し、整形したデータを返却
-    */
-    function dataAnalyze($data){
-        // ','でデータが区切ってあるので、データを分割
-        list($user, $vote1, $vote2, $vote3, $date) = explode(",", $data);
-        //ユーザのデータがなければデータを挿入 OR ユーザの投票データがあって、今回のデータの方が最新の場合、データの更新
-        if(!isset($result[$user]) || (isset($result[$user]) && strtotime($date) > strtotime($result[$user]["date"]))){
-            echo "update: ". $user . ", " . $vote1 . ", " . $vote2 . ", " . $vote3 . ", " . $date . "<br>";
-            return array("user" => $user, "vote1" => $vote1, "vote2" => $vote2, "vote3" => $vote3, "date" => $date);
-        }
-    }
+    include_once("FileMake.php");
+
+    $fm = new FileMake();
 
     //ファイルが選択されていない場合
     if(!isset($_POST["file"])){
@@ -24,30 +13,11 @@
 
     if(isset($_POST["file"])){
         $result = array();
-        //ファイルの数だけループを回し、$resultにユーザのデータを格納していく
-        foreach($_POST["file"] as $key => $val){
-            $file = fopen(TARGET_DIR.$val, "r");
-            //ファイルがある場合は、1行ずつ読込
-            if($file){
-                while($line = fgets($file)){
-                    // ','でデータが区切ってあるので、データを分割
-                    list($user, $vote1, $vote2, $vote3, $date) = explode(",", $line);
-                    //ユーザのデータがなければデータを挿入 OR ユーザの投票データがあって、今回のデータの方が最新の場合、データの更新
-                    if(!isset($result[$user]) || (isset($result[$user]) && strtotime($date) > strtotime($result[$user]["date"]))){
-                        $result[$user] =  array("user" => $user, "vote1" => $vote1, "vote2" => $vote2, "vote3" => $vote3, "date" => $date);
-                    }
-                }
-            }
-            fclose($file);
-        }
-        touch(DIST_FILE);
-        $current = "";
-        //データの取得
-        foreach ($result as $key => $value) {
-            $current .= $value["user"] . "," . $value["vote1"] . "," . $value["vote2"] . "," . $value["vote3"] . "\n";
-        }
-        //ファイルに書き込み
-        file_put_contents(DIST_FILE, $current);
+        //ファイルの中の全ユーザの最新データ取得
+        $result = $fm->getUserData($_POST["file"]);
+
+        //ダウンロードしてもらうためのファイルの作成(最新のユーザのデータをファイルに書き込み)
+        $fm->writeLatestUserData(DIST_FILE, $result);
 
         //保存させる際のファイル名
         $j_file = "投票結果.csv";
